f5b131b14ceb833e1662d32f9cfde6d015a2606d
EXRAZER <EXRAZER@icloud.com>
2024-01-03 09:38:04 +0700

    config test

--- tests/seven-eleven/test_7_eleven.py
+++ tests/seven-eleven/test_7_eleven.py
@@ -7,6 +7,29 @@ import pytest
 
 #project library
 #from seven_eleven.secen from print_7_eleven
+@pytest.mark.parametrize(
+    "number, expected"
+    [
+        (1, 1),
+        (2, 3),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, "seven"),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+        (11, "eleven"),
+        (14, "seven"),
+        (21, "seven"),
+        (21, "seven")
+    ]
+)
 
-def test_7_eleven():
+def test_7_eleven(number, expected):
     """print 7-eleven"""
+    #Arrang
+    #Act
+    #Assert
+    
