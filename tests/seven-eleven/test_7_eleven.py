54f5a76482aa06d5128a93f4b070623ca0a4b68a
EXRAZER <EXRAZER@icloud.com>
2024-01-03 09:09:49 +0700

    Add 7-eleven

---
+++ tests/seven-eleven/test_7_eleven.py
@@ -0,0 +1,12 @@
+"""Test cases for 7-Eleven."""
+
+#Standard library
+
+#3rd party library
+import pytest
+
+#project library
+#from seven_eleven.secen from print_7_eleven
+
+def test_7_eleven():
+    """print 7-eleven"""
